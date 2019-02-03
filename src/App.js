b01ea01edc9a59e3912a340cf55e9fec4fa5970d
Pyry Kanerva <kanervapyry@gmail.com>
2019-02-03 17:43:19 +0200

    fetch markers from local state, store user markers to db. Removed Marker component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 import Map from "./Components/Map";
-import Marker from "./Components/Marker";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Map>
-          <Marker
-            id="myLocation"
-            message="This is a placeholder message"
-            coords={[24.9383, 60.1691]}
-            color="blue"
-          />
-          <Marker
-            id="anotherLocation"
-            coords={[13.339826, 52.488932]}
-            color="orange"
-          />
-        </Map>
+        <Map />
       </div>
     );
   }
--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,14 +1,63 @@
 import React from "react";
 import mapboxGl from "mapbox-gl";
+import axios from "axios";
 
 class Map extends React.Component {
   state = {
-    map: null
+    map: null,
+
+    helsinkiArea: null,
+    geojson: {
+      type: "geojson",
+      data: {
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [24.9384, 60.161]
+            },
+            properties: {
+              title: "Mapbox",
+              message: "Washington, D.C."
+            }
+          },
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [24.9384, 60.1699]
+            },
+            properties: {
+              title: "Mapbox",
+              message: "San Francisco, California"
+            }
+          }
+        ]
+      }
+    }
   };
 
   componentDidMount() {
+    // axios
+    //   .get("https://goecache-e75b9.firebaseio.com/markers.json")
+    //   .then(res => {
+    //     console.log(res);
+    //     console.log(res.data);
+    //     this.setState({ geojson: res.data });
+    //   });
+
+    axios
+      .get("https://goecache-e75b9.firebaseio.com/helsinki.json")
+      .then(res => {
+        console.log(res.data);
+        this.setState({ helsinkiArea: res.data });
+      });
+
     mapboxGl.accessToken =
       "pk.eyJ1IjoicHlyeWthIiwiYSI6ImNqcmNjc3F5cTBqZ3U0YW1sN2U0NWdwaGEifQ.ObuMlu8asDW2CCOLDmf-Kg";
+
     const map = new mapboxGl.Map({
       container: this.mapContainer,
       style: "mapbox://styles/mapbox/streets-v9",
@@ -19,31 +68,85 @@ class Map extends React.Component {
     map.on("load", (...args) => {
       this.setState({ map });
 
-      //Change cursor to pointer on mouse enter
-      map.on("mouseenter", "markers", function() {
+      console.log(this.state.geojson);
+
+      map.addSource("markers", this.state.geojson);
+
+      //map.addSource("helsinki", this.state.helsinkiArea);
+
+      map.addLayer({
+        id: "markers",
+        type: "circle",
+        source: "markers",
+        paint: {
+          "circle-radius": 10,
+          "circle-color": "red"
+        }
+      });
+
+      // map.addLayer({
+      //   id: "park-boundary",
+      //   type: "fill",
+      //   source: this.state.helsinkiArea,
+      //   paint: {
+      //     "fill-color": "#888888",
+      //     "fill-opacity": 0.4
+      //   },
+      //   filter: ["==", "$type", "Polygon"]
+      // });
+
+      //Create the popup for showing messages
+      const popup = new mapboxGl.Popup({
+        closeButton: false,
+        closeOnClick: false
+      });
+
+      map.on("click", function(e) {
+        console.log(e.lngLat);
+        axios.post(
+          "https://goecache-e75b9.firebaseio.com/markers/data/features.json",
+          {
+            type: "Feature",
+            geometry: {
+              coordinates: [e.lngLat],
+              type: "Point"
+            },
+            properties: {
+              message: "Placeholder"
+            }
+          }
+        );
+      });
+
+      //Change cursor to pointer on mouse enter and show popup
+      map.on("mouseenter", "markers", function(e) {
         map.getCanvas().style.cursor = "pointer";
+
+        let coordinates = e.features[0].geometry.coordinates.slice();
+        let message = e.features[0].properties.message;
+
+        //if multiple features visible, show popup on the one that is poited at
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        popup
+          .setLngLat(coordinates)
+          .setHTML(message)
+          .addTo(map);
       });
 
       //Change cursor back on mouse leave
       map.on("mouseleave", "markers", function() {
         map.getCanvas().style.cursor = "";
-      });
-
-      map.on("click", "markers", function(e) {
-        let coordinates = e.features[0].geometry.coordinates.slice();
-        let message = e.features[0].properties.message;
-
-        new mapboxGl.Popup()
-          .setLngLat(coordinates)
-          .setHTML(message)
-          .addTo(map);
+        popup.remove();
       });
     });
   }
 
   render() {
     const style = {
-      height: "500px",
+      height: "800px",
       width: "100%"
     };
 
